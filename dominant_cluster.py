c84f5342fae79af0779010d77b20b0e6d5fd80a5
CoderHam <hemanham@gmail.com>
2020-12-21 17:32:01 -0800

    Add support for gpu accelerated kmeans

--- dominant_cluster.py
+++ dominant_cluster.py
@@ -1,55 +1,98 @@
+#! /usr/bin/env python3
+import faiss
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+import image_utils
+
 import numpy as np
-import cv2
+from collections import Counter
 
-def get_dom_colors(image_path,clusters=10,plot=False):
-	image = cv2.imread(image_path)
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	# flatten to 1d
-	image = image.reshape((image.shape[0] * image.shape[1], 3))
-	# cluster the pixel intensities
-	clt = KMeans(n_clusters = clusters)
-	clt.fit(image)
-	hist = centroid_histogram(clt)
+def kmeans_faiss(dataset, k):
+    "Runs KMeans on GPU/s"
+    dims = dataset.shape[1]
+    cluster = faiss.Clustering(dims, k)
+    cluster.verbose = False
+    cluster.niter = 20
+    cluster.max_points_per_centroid = 10 ** 7
 
-	dcolors = [c.astype("uint8").tolist() for c in clt.cluster_centers_]
-	print(dcolors)
-	if plot:
-		bar = plot_colors(hist, clt.cluster_centers_)
-		plt.figure()
-		plt.axis("off")
-		plt.imshow(bar)
-		plt.show()
+    resources = faiss.StandardGpuResources()
+    config = faiss.GpuIndexFlatConfig()
+    config.useFloat16 = False
+    config.device = 0
+    index = faiss.GpuIndexFlatL2(resources, dims, config)
 
-	return dcolors
+    # perform kmeans
+    cluster.train(dataset, index)
+    centroids = faiss.vector_float_to_array(cluster.centroids)
 
+    return centroids.reshape(k, dims)
 
-def plot_colors(hist, centroids):
-
-	bar = np.zeros((50, 300, 3), dtype = "uint8")
-	startX = 0
-
-	for (percent, color) in zip(hist, centroids):
-		endX = startX + (percent * 300)
-		cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-			color.astype("uint8").tolist(), -1)
-		startX = endX
-
-	return bar
 
 def centroid_histogram(clt):
-	numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-	(hist, _) = np.histogram(clt.labels_, bins=numLabels)
+    num_labels = np.arange(0, len(np.unique(clt.labels_)) + 1)
 
-	hist = hist.astype("float")
-	hist /= hist.sum()
+    (hist, _) = np.histogram(clt.labels_, bins=num_labels)
 
-	return hist
+    hist = hist.astype("float")
+    hist /= hist.sum()
 
-# bar = plot_colors(hist, clt.cluster_centers_)
+    return hist
 
-# plt.figure()
-# plt.axis("off")
-# plt.imshow(bar)
-# plt.show()
+
+def get_dom_colors(image_path, n_clusters=10, use_gpu=True, plot=True):
+    image = image_utils.load_image(image_path, resize=True)
+    image = image.reshape((image.shape[0] * image.shape[1], 3)).astype('float32')
+
+    if use_gpu:
+        centroids = kmeans_faiss(image, n_clusters)
+        if plot:
+            labels = compute_cluster_assignment(centroids, image)
+
+            centroids = (centroids*255).astype("uint8")
+            counts = Counter(labels).most_common()
+            total = sum(n for _, n in counts)
+            centroid_size_tuples = [(centroids[k], val/total) for k, val in counts]
+    else:
+        clt = KMeans(n_clusters=n_clusters)
+        clt.fit(image)
+        centroids = np.array([(center*255).astype("uint8").tolist() for center in clt.cluster_centers_])
+        
+        if plot:
+            hist = centroid_histogram(clt)
+            centroid_size_tuples = list(zip(centroids, hist))
+            centroid_size_tuples.sort(key=lambda x: x[1], reverse=True)
+    if plot:
+        bar_image = image_utils.bar_colors(centroid_size_tuples)
+        return centroids, bar_image
+
+    return (centroids*255).astype("uint8")
+
+
+def compute_cluster_assignment(centroids, x):
+    dims = centroids.shape[1]
+
+    resources = faiss.StandardGpuResources()
+    config = faiss.GpuIndexFlatConfig()
+    config.useFloat16 = False
+    config.device = 0
+
+    index = faiss.GpuIndexFlatL2(resources, dims, config)
+    index.add(centroids)
+    _, labels = index.search(x, 1)
+
+    return labels.ravel()
+
+
+# import time
+# t0 = time.time()
+# dom_colors, bar = get_dom_colors("images/dancing.jpg", n_clusters=10)
+# t1 = time.time()
+# print(dom_colors)
+# image_utils.save_plot(bar, "test1.png")
+# print("k-Means runtime: %.3f s" % (t1 - t0))
+
+# t0 = time.time()
+# dom_colors, bar = get_dom_colors("images/dancing.jpg", n_clusters=10, use_gpu=False)
+# t1 = time.time()
+# print(dom_colors)
+# image_utils.save_plot(bar, "test.png")
+# print("k-Means runtime: %.3f s" % (t1 - t0))
---
+++ image_utils.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+import numpy as np
+from skimage import io, transform
+import cv2
+
+def load_image(img_path, resize=False):
+    tmp_img = io.imread(img_path)
+    if resize:
+        return transform.resize(image=tmp_img,output_shape=(200,200),anti_aliasing=True, mode='constant')
+    return tmp_img
+
+
+def bar_colors(centroid_size_tuples):
+	bar = np.zeros((50, 300, 3), dtype = "uint8")
+	x_start = 0
+	for (color, percent) in centroid_size_tuples:
+		x_end = x_start + (percent * 300)
+		cv2.rectangle(bar, (int(x_start), 0), (int(x_end), 50),
+			color.astype("uint8").tolist(), -1)
+		x_start = x_end
+	return bar
+
+
+def save_plot(plot, image_path):
+    io.imsave(image_path, plot)
+
+
