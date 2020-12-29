Fix bug in dominant color calculation

dominant_cluster.py
@@ -27,45 +27,6 @@ def kmeans_faiss(dataset, k):
     return centroids.reshape(k, dims)
 
 
-def centroid_histogram(clt):
-    num_labels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-
-    (hist, _) = np.histogram(clt.labels_, bins=num_labels)
-
-    hist = hist.astype("float")
-    hist /= hist.sum()
-
-    return hist
-
-
-def get_dominant_colors(image, n_clusters=10, use_gpu=True, plot=True):
-    image = image.reshape((image.shape[0] * image.shape[1], 3)).astype('float32')
-
-    if use_gpu:
-        centroids = kmeans_faiss(image, n_clusters)
-        if plot:
-            labels = compute_cluster_assignment(centroids, image)
-
-            centroids = (centroids*255).astype("uint8")
-            counts = Counter(labels).most_common()
-            total = sum(n for _, n in counts)
-            centroid_size_tuples = [(centroids[k], val/total) for k, val in counts]
-    else:
-        clt = KMeans(n_clusters=n_clusters)
-        clt.fit(image)
-        centroids = np.array([(center*255).astype("uint8").tolist() for center in clt.cluster_centers_])
-        
-        if plot:
-            hist = centroid_histogram(clt)
-            centroid_size_tuples = list(zip(centroids, hist))
-            centroid_size_tuples.sort(key=lambda x: x[1], reverse=True)
-    if plot:
-        bar_image = image_utils.bar_colors(centroid_size_tuples)
-        return centroids, bar_image
-
-    return (centroids*255).astype("uint8")
-
-
 def compute_cluster_assignment(centroids, data):
     dims = centroids.shape[1]
 
@@ -79,6 +40,44 @@ def compute_cluster_assignment(centroids, data):
     _, labels = index.search(data, 1)
 
     return labels.ravel()
+
+
+def centroid_histogram(clt):
+    num_labels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+
+    (hist, _) = np.histogram(clt.labels_, bins=num_labels)
+    hist = hist.astype(np.float32)
+    hist /= hist.sum()
+
+    return hist
+
+
+def get_dominant_colors(image, n_clusters=10, use_gpu=True, plot=True):
+    image = image.reshape((image.shape[0] * image.shape[1], 3)).astype(np.float32)
+
+    if use_gpu:
+        centroids = kmeans_faiss(image, n_clusters)
+        labels = compute_cluster_assignment(centroids, image)
+        centroids = centroids.astype("uint8")
+
+        if plot:
+            counts = Counter(labels).most_common()
+            total = sum(n for _, n in counts)
+            centroid_size_tuples = [(centroids[k], val/total) for k, val in counts]
+    else:
+        clt = KMeans(n_clusters=n_clusters)
+        clt.fit(image)
+        centroids = clt.cluster_centers_.astype(np.uint8)
+
+        if plot:
+            hist = centroid_histogram(clt)
+            centroid_size_tuples = list(zip(centroids, hist))
+            centroid_size_tuples.sort(key=lambda x: x[1], reverse=True)
+    if plot:
+        bar_image = image_utils.bar_colors(centroid_size_tuples)
+        return centroids, bar_image
+
+    return centroids
 
 
 # import time